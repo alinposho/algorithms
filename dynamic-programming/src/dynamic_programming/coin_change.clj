ed24d2f8cf98ce0ad0f79e2812088d839b47c66e
Alin Posorovaschi <alin.posorovaschi@onthemarket.com>
2020-04-22 00:36:06 +0300

    Add DP solution to the coin change problem

--- dynamic-programming/src/dynamic_programming/coin_change.clj
+++ dynamic-programming/src/dynamic_programming/coin_change.clj
@@ -5,18 +5,66 @@
 (defn coin-change-count-recursive
   "Naive solution, i.e. non DP, to the coin change problem."
   [[coin & rst :as coins] amount]
-   (cond
-     (zero? amount) 1
-     (< amount 0) 0
-     (and (nil? coin) (< 0 amount)) 0
-     :else (+ (coin-change-count-recursive rst amount)
-              (coin-change-count-recursive coins (- amount coin)))))
+  (cond
+    (zero? amount) 1
+    (< amount 0) 0
+    (and (nil? coin) (< 0 amount)) 0
+    :else (+ (coin-change-count-recursive rst amount)
+             (coin-change-count-recursive coins (- amount coin)))))
 
-(comment
 
-  (coin-change-count-recursive [] 9)
-  (coin-change-count-recursive nil 9)
+(def coin-change-count-memoized
+  (fn [[coin & rst :as coins] amount]
+    (cond
+      (zero? amount) 1
+      (< amount 0) 0
+      (and (nil? coin) (< 0 amount)) 0
+      :else (+ (coin-change-count-memoized rst amount)
+               (coin-change-count-memoized coins (- amount coin))))))
 
-  (coin-change-count-recursive [1 2 3] 4)
-  (coin-change-count-recursive [2 5 3 6] 10)
-  )
+(defn coin-change-count-dp
+  "Dynamic Programming solution to the coin change problem"
+  ([coins amount]
+   (let [table (vec (cons 1 (repeat amount 0)))]
+     (coin-change-count-dp coins amount table)))
+  ([[coin & rst] amount known-solutions]
+   (if-not coin
+     (last known-solutions)
+     (let [new-known-solutions (reduce (fn [acc c] (update acc c + (acc (- c coin))))
+                                       known-solutions
+                                       (range coin (inc amount)))]
+       (recur rst amount new-known-solutions)))))
+
+  (comment
+
+    (coin-change-count-recursive [] 9)
+    (coin-change-count-recursive nil 9)
+
+    (coin-change-count-recursive [1 2 3] 4)
+    (coin-change-count-recursive [1 2 3] 100)
+    (coin-change-count-recursive [2 5 3 6] 10)
+
+    ;; Notice there is no significant change in the execution time for the following inputs
+    (time (coin-change-count-recursive [2 5 3 6] 500))
+    (time (coin-change-count-memoized [2 5 3 6] 500))
+
+    (-> (range 1 30)
+        (coin-change-count-recursive 50)
+        time)
+
+    (-> (range 1 30)
+        (coin-change-count-memoized 50)
+        time)
+
+
+    (coin-change-count-dp [] 9)
+    (coin-change-count-dp nil 9)
+    (coin-change-count-dp #{11 12} 9)
+    (coin-change-count-dp #{1 3} 4)
+
+    ;; Notice DP solution is quite fast compare to the others
+    (time (coin-change-count-dp #{2 5 3 6} 500))
+    (time (coin-change-count-dp #{2 5 3 6} 1000))
+
+
+    )
--- dynamic-programming/test/dynamic_programming/coin_change_test.clj
+++ dynamic-programming/test/dynamic_programming/coin_change_test.clj
@@ -5,16 +5,35 @@
 (deftest coin-change-count-recursive-test
   (testing "coin-change-count-recursive given empty coins should return zero"
     (is (zero? (sut/coin-change-count-recursive nil 7)))
-    (is (zero? (sut/coin-change-count-recursive [] 7))))
+    (is (zero? (sut/coin-change-count-recursive #{} 7))))
 
   (testing "coin-change-count-recursive given no change possible should return zero"
-    (is (zero? (sut/coin-change-count-recursive [3 5 6] 7)))
-    (is (zero? (sut/coin-change-count-recursive [3 5 11 9] 4)))
-    (is (zero? (sut/coin-change-count-recursive [3 5 11 9] 2))))
+    (is (zero? (sut/coin-change-count-recursive #{3 5 6} 7)))
+    (is (zero? (sut/coin-change-count-recursive #{3 5 11 9} 4)))
+    (is (zero? (sut/coin-change-count-recursive #{3 5 11 9} 2))))
 
   (testing "coin-change-count-recursive given trivial cases should return one"
-    (is (= 1 (sut/coin-change-count-recursive [7] 7))))
+    (is (= 1 (sut/coin-change-count-recursive #{7} 7))))
 
   (testing "coin-change-count-recursive given coins and possible solutions should return their count"
-    (is (= 4 (sut/coin-change-count-recursive [1 2 3] 4)))
-    (is (= 5 (sut/coin-change-count-recursive [2 5 3 6] 10)))))
+    (is (= 4 (sut/coin-change-count-recursive #{1 2 3} 4)))
+    (is (= 5 (sut/coin-change-count-recursive #{2 5 3 6} 10)))))
+
+(deftest coin-change-count-dp-test
+  (testing "coin-change-count-dp given empty coins should return zero"
+    (is (zero? (sut/coin-change-count-dp nil 7)))
+    (is (zero? (sut/coin-change-count-dp #{} 7))))
+
+  (testing "coin-change-count-dp given no change possible should return zero"
+    (is (zero? (sut/coin-change-count-dp #{3 5 6} 7)))
+    (is (zero? (sut/coin-change-count-dp #{3 5 11 9} 4)))
+    (is (zero? (sut/coin-change-count-dp #{3 5 11 9} 2))))
+
+  (testing "coin-change-count-dp given trivial cases should return one"
+    (is (= 1 (sut/coin-change-count-dp #{7} 7))))
+
+  (testing "coin-change-count-dp given coins and possible solutions should return their count"
+    (is (= 4 (sut/coin-change-count-dp #{1 2 3} 4)))
+    (is (= 5 (sut/coin-change-count-dp #{2 5 3 6} 10)))
+    (is (= (sut/coin-change-count-recursive #{2 5 3 6} 100) (sut/coin-change-count-dp #{2 5 3 6} 100)))
+    (is (= 928149593 (sut/coin-change-count-dp #{2 5 3 6} 10000)))))
